89ad7053190c73306c1bafd898a8a590ddb62bad
Javier Aceves <jaacevesh@gmail.com>
2022-02-16 23:15:11 -0600

    tests

---
+++ contracts/Dex.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+pragma abicoder v2;
+import './Wallet.sol';
+
+contract Dex is Wallet {
+
+    enum Side {
+        BUY,
+        SELL
+    }
+
+    struct Order {
+        uint id;
+        address trader;
+        bool buyOrder;
+        bytes32 ticker;
+        uint amount;
+        uint price;
+    }
+
+    mapping(bytes32 => mapping(uint => Order[])) public orderBook;
+    
+
+    function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory){
+        return orderBook[ticker][uint(side)];
+    }
+
+    /* function createLimitOrder() public {
+
+    } */
+
+}
